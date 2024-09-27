5b23d2fc293995f720e72efe82c79c6ddc469a3f
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-27 15:37:37 +0300

    Setting up,
    I have set the project structure folders and paths

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,17 +1,7 @@
-import Counter from "../components/Counter";
-
 async function page() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data = await res.json();
   return (
     <div>
       <h2>Cabins</h2>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
