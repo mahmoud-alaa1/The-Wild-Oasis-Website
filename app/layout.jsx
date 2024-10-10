ff6d32fdaaacc259d644bd838c746d790d04d504
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-10 18:29:47 +0300

    Refactor metadata titles in page components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,15 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
+import Header from "@/starter/components/Header";
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -16,13 +24,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
