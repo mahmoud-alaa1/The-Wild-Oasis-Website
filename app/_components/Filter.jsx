54b962a9051cb66113a25782ef2e3e472663612e
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-19 13:45:32 +0300

    Refactor Filter component to update URL parameters when filtering by capacity

--- app/_components/Filter.jsx
+++ app/_components/Filter.jsx
@@ -1,11 +1,19 @@
 "use client";
 
+const filters = [
+  { value: "all", label: "All cabins" },
+  { value: "small", label: "1 - 3 guests" },
+  { value: "medium", label: "4 - 7 guests" },
+  { value: "large", label: "8 - 12 guests" },
+];
+
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 function Filter() {
   const searchparams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
+  const activeFilter = searchparams.get("capacity") ?? "all";
   function handleFileter(filter) {
     const params = new URLSearchParams(searchparams);
     params.set("capacity", filter);
@@ -14,19 +22,20 @@ function Filter() {
   }
   return (
     <div className="border border-primary-800 flex">
-      <button onClick={() => handleFileter("all")} className="px-5 py-2 hover:bg-primary-700">
-        All cabins
-      </button>
-      <button onClick={() => handleFileter("small")} className="px-5 py-2 hover:bg-primary-700">
-        1&mdash;3 guests
-      </button>
-      <button onClick={() => handleFileter("medium")} className="px-5 py-2 hover:bg-primary-700">
-        4&mdash;7 guests
-      </button>
-      <button onClick={() => handleFileter("large")} className="px-5 py-2 hover:bg-primary-700">
-        8&mdash;12 guests
-      </button>
+      {filters.map((filter) => (
+        <Button key={filter.value} onClick={() => handleFileter(filter.value)} active={activeFilter === filter.value}>
+          {filter.label}
+        </Button>
+      ))}
     </div>
+  );
+}
+
+function Button({ children, onClick, active }) {
+  return (
+    <button onClick={onClick} className={`px-5 py-2 hover:bg-primary-700 ${active ? "bg-primary-700" : ""}`}>
+      {children}
+    </button>
   );
 }
 
