7d201fca9dbea0c0bf84c9371c2b99ec22983a6e
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-20 07:32:19 +0300

    Refactor DateSelector and cabin reservation page components

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,14 +1,12 @@
+"use client";
+
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
 function isAlreadyBooked(range, datesArr) {
   return (
-    range.from &&
-    range.to &&
-    datesArr.some((date) =>
-      isWithinInterval(date, { start: range.from, end: range.to })
-    )
+    range.from && range.to && datesArr.some((date) => isWithinInterval(date, { start: range.from, end: range.to }))
   );
 }
 
@@ -44,9 +42,7 @@ function DateSelector() {
             {discount > 0 ? (
               <>
                 <span className="text-2xl">${regularPrice - discount}</span>
-                <span className="line-through font-semibold text-primary-700">
-                  ${regularPrice}
-                </span>
+                <span className="line-through font-semibold text-primary-700">${regularPrice}</span>
               </>
             ) : (
               <span className="text-2xl">${regularPrice}</span>
@@ -67,10 +63,7 @@ function DateSelector() {
         </div>
 
         {range.from || range.to ? (
-          <button
-            className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
-          >
+          <button className="border border-primary-800 py-2 px-4 text-sm font-semibold" onClick={() => resetRange()}>
             Clear
           </button>
         ) : null}
